0ade124055ff267174b4976c755349fb6d83a97b
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-05-10 20:11:24 +1000

    make smithing types avoid tags. use minecraft namespace.

--- src/main/java/folk/sisby/tinkerers_smithing/data/SmithingTypeLoader.java
+++ src/main/java/folk/sisby/tinkerers_smithing/data/SmithingTypeLoader.java
@@ -15,15 +15,18 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.Registry;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.quiltmc.qsl.resource.loader.api.reloader.IdentifiableResourceReloader;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SmithingTypeLoader extends MultiJsonDataLoader implements IdentifiableResourceReloader {
 	public static final SmithingTypeLoader INSTANCE = new SmithingTypeLoader(new Gson());
 	public static final Identifier ID = new Identifier(TinkerersSmithing.ID, "smithing_type_loader");
-	public static final TagGroupLoader<Item> itemTagLoader = new TagGroupLoader<>(Registry.ITEM::getOrEmpty, "tags/items");
+	public static final TagGroupLoader<Item> ITEM_TAG_LOADER = new TagGroupLoader<>(Registry.ITEM::getOrEmpty, "tags/items");
+	public static final String AVOIDANCE_PREFIX = "tinkerers_smithing_types/";
 
 	public SmithingTypeLoader(Gson gson) {
 		super(gson, "smithing_types");
@@ -35,7 +38,7 @@ public class SmithingTypeLoader extends MultiJsonDataLoader implements Identifia
 	}
 
 	public static void addToTag(Map<Identifier, Collection<Item>> tags, String path, Item item) {
-		Identifier id = new Identifier(TinkerersSmithing.ID, path);
+		Identifier id = new Identifier(path); // minecraft for slot stuff
 		HashSet<Item> mutable = new HashSet<>(tags.computeIfAbsent(id, k -> new HashSet<>()));
 		mutable.add(item);
 		tags.put(id, ImmutableList.copyOf(mutable));
@@ -45,8 +48,9 @@ public class SmithingTypeLoader extends MultiJsonDataLoader implements Identifia
 	protected void apply(Map<Identifier, Collection<Pair<JsonElement, String>>> prepared, ResourceManager manager, Profiler profiler) {
 		Map<Identifier, List<TagGroupLoader.EntryWithSource>> typeTags = new HashMap<>();
 		prepared.forEach((id, jsons) -> {
+			Identifier collisionAvoidingID = new Identifier(id.getNamespace(), AVOIDANCE_PREFIX + id.getPath());
 			jsons.forEach(jsonEntry -> {
-				List<TagGroupLoader.EntryWithSource> list = typeTags.computeIfAbsent(id, k -> new ArrayList<>());
+				List<TagGroupLoader.EntryWithSource> list = typeTags.computeIfAbsent(collisionAvoidingID, k -> new ArrayList<>());
 				TagFile tagFile = TagFile.CODEC.parse(new Dynamic<>(JsonOps.INSTANCE, jsonEntry.getLeft())).getOrThrow(false, TinkerersSmithing.LOGGER::error);
 				if (tagFile.replace()) {
 					list.clear();
@@ -55,23 +59,25 @@ public class SmithingTypeLoader extends MultiJsonDataLoader implements Identifia
 			});
 		});
 
-		Map<Identifier, List<TagGroupLoader.EntryWithSource>> itemTags = itemTagLoader.loadTags(manager);
+		Map<Identifier, List<TagGroupLoader.EntryWithSource>> itemTags = ITEM_TAG_LOADER.loadTags(manager);
 		Map<Identifier, List<TagGroupLoader.EntryWithSource>> allTags = new HashMap<>();
 		allTags.putAll(itemTags);
 		allTags.putAll(typeTags);
-		Map<Identifier, Collection<Item>> tags = itemTagLoader.build(allTags);
+		Map<Identifier, Collection<Item>> tags = ITEM_TAG_LOADER.build(allTags);
 		tags.entrySet().removeIf(e -> !typeTags.containsKey(e.getKey()));
+		// Strip collision avoiding ID
+		tags = tags.entrySet().stream().collect(Collectors.toMap(e -> new Identifier(e.getKey().getNamespace(), StringUtils.removeStart(e.getKey().getPath(), AVOIDANCE_PREFIX)), Map.Entry::getValue));
 		// Manually jam in stuff by equipment slot, false positives should wash out by having no material.
-		Registry.ITEM.forEach(item -> {
+		for (Item item : Registry.ITEM) {
 			if (item instanceof ArmorItem ai) {
 				switch (ai.getSlotType()) {
-					case FEET ->  addToTag(tags, "boots", item);
+					case FEET -> addToTag(tags, "boots", item);
 					case LEGS -> addToTag(tags, "leggings", item);
 					case CHEST -> addToTag(tags, "chestplate", item);
 					case HEAD -> addToTag(tags, "helmet", item);
 				}
 			}
-		});
+		}
 		TinkerersSmithing.SMITHING_TYPES.clear();
 		TinkerersSmithing.SMITHING_TYPES.putAll(tags);
 	}
