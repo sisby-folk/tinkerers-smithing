b9513d90ad0d73f41793ad103475438dfb5a7f83
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-05-11 17:21:42 +1000

    add emi anvil repair recipes

--- src/main/java/folk/sisby/tinkerers_smithing/client/emi/TinkerersSmithingPlugin.java
+++ src/main/java/folk/sisby/tinkerers_smithing/client/emi/TinkerersSmithingPlugin.java
@@ -2,10 +2,17 @@ package folk.sisby.tinkerers_smithing.client.emi;
 
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.recipe.EmiAnvilRecipe;
 import folk.sisby.tinkerers_smithing.TinkerersSmithingItemData;
 import folk.sisby.tinkerers_smithing.client.TinkerersSmithingClient;
 import folk.sisby.tinkerers_smithing.client.emi.recipe.EmiAnvilDeworkRecipe;
+import folk.sisby.tinkerers_smithing.client.emi.recipe.EmiAnvilRepairRecipe;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.recipe.Ingredient;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TinkerersSmithingPlugin implements EmiPlugin {
 	@Override
@@ -13,6 +20,13 @@ public class TinkerersSmithingPlugin implements EmiPlugin {
 		for (TinkerersSmithingItemData itemData : TinkerersSmithingClient.SERVER_SMITHING_ITEMS.values()) {
 			if (Enchantments.VANISHING_CURSE.isAcceptableItem(itemData.item().getDefaultStack())) {
 				registry.addRecipe(new EmiAnvilDeworkRecipe(itemData.item()));
+			}
+			if (!itemData.unitCosts().isEmpty()) {
+				Map<Integer, List<Ingredient>> invertedCosts = itemData.unitCosts().entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toList())));
+				invertedCosts.forEach((cost, ingredients) -> {
+					registry.removeRecipes(r -> r instanceof EmiAnvilRecipe ear && ear.getOutputs().stream().allMatch(es -> es.getItemStack().isOf(itemData.item())));
+					registry.addRecipe(new EmiAnvilRepairRecipe(itemData.item(), ingredients));
+				});
 			}
 		}
 	}
---
+++ src/main/java/folk/sisby/tinkerers_smithing/client/emi/recipe/EmiAnvilRepairRecipe.java
@@ -0,0 +1,89 @@
+package folk.sisby.tinkerers_smithing.client.emi.recipe;
+
+import dev.emi.emi.EmiUtil;
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
+import dev.emi.emi.api.render.EmiTexture;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.WidgetHolder;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Random;
+
+public class EmiAnvilRepairRecipe implements EmiRecipe {
+	private final Item tool;
+	private final EmiIngredient repairMaterial;
+	private final int uniq = EmiUtil.RANDOM.nextInt();
+
+	public EmiAnvilRepairRecipe(Item tool, List<Ingredient> ingredients) {
+		this.tool = tool;
+		this.repairMaterial = EmiIngredient.of(ingredients.stream().map(EmiIngredient::of).toList());
+	}
+
+	@Override
+	public EmiRecipeCategory getCategory() {
+		return VanillaEmiRecipeCategories.ANVIL_REPAIRING;
+	}
+
+	@Override
+	public @Nullable Identifier getId() {
+		return null;
+	}
+
+	@Override
+	public List<EmiIngredient> getInputs() {
+		return List.of(EmiStack.of(tool), repairMaterial);
+	}
+
+	@Override
+	public List<EmiStack> getOutputs() {
+		return List.of(EmiStack.of(tool));
+	}
+
+	@Override
+	public boolean supportsRecipeTree() {
+		return false;
+	}
+
+	@Override
+	public int getDisplayWidth() {
+		return 125;
+	}
+
+	@Override
+	public int getDisplayHeight() {
+		return 18;
+	}
+
+	@Override
+	public void addWidgets(WidgetHolder widgets) {
+		widgets.addTexture(EmiTexture.PLUS, 27, 3);
+		widgets.addTexture(EmiTexture.EMPTY_ARROW, 75, 1);
+		widgets.addGeneratedSlot(r -> getTool(r, false), uniq, 0, 0);
+		widgets.addSlot(repairMaterial, 49, 0);
+		widgets.addGeneratedSlot(r -> getTool(r, true), uniq, 107, 0).recipeContext(this);
+	}
+
+	private EmiStack getTool(Random r, boolean repaired) {
+		ItemStack stack = tool.getDefaultStack().copy();
+		if (stack.getMaxDamage() <= 0) {
+			return EmiStack.of(tool);
+		}
+		int d = r.nextInt(stack.getMaxDamage());
+		if (repaired) {
+			d -= stack.getMaxDamage() / 4;
+			if (d <= 0) {
+				return EmiStack.of(tool);
+			}
+		}
+		stack.setDamage(d);
+		return EmiStack.of(stack);
+	}
+}
