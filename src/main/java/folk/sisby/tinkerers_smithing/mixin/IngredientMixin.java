8d3dc3c8ee9836c96e99ebe28086364e1f2df617
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-01-29 12:59:45 +1100

    drop unneeded mixins

--- src/main/java/folk/sisby/tinkerers_smithing/mixin/IngredientMixin.java
+++ src/main/java/folk/sisby/tinkerers_smithing/mixin/IngredientMixin.java
@@ -1,23 +1,19 @@
 package folk.sisby.tinkerers_smithing.mixin;
 
 import folk.sisby.tinkerers_smithing.TinkerersSmithing;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Ingredient.class)
 public class IngredientMixin {
 	@Unique private boolean isFootgun() {
-		if (Registry.ITEM.streamTagsAndEntries().toList().isEmpty()) {
+		if (Registries.ITEM.streamTagsAndEntries().toList().isEmpty()) {
 			TinkerersSmithing.LOGGER.error("[Tinkerer's Smithing] Cowardly refusing to access ingredient while tags are unloaded", new IllegalStateException("An ingredient was accessed before item tags are loaded - This would normally break all tag recipes! Please report this to the mod in the trace below."));
 			return true;
 		}
@@ -29,30 +25,6 @@ public class IngredientMixin {
 		if (isFootgun()) {
 			cir.setReturnValue(new ItemStack[]{});
 			cir.cancel();
-		}
-	}
-
-	@Inject(method = "test(Lnet/minecraft/item/ItemStack;)Z", at = @At("HEAD"), cancellable = true)
-	public void test(CallbackInfoReturnable<Boolean> cir) {
-		if (isFootgun()) {
-			cir.setReturnValue(false);
-			cir.cancel();
-		}
-	}
-
-	@Inject(method = "getMatchingItemIds", at = @At("HEAD"), cancellable = true)
-	public void getMatchingItemIds(CallbackInfoReturnable<IntList> cir) {
-		if (isFootgun()) {
-			cir.setReturnValue(new IntArrayList());
-			cir.cancel();
-		}
-	}
-
-	@Inject(method = "write", at = @At("HEAD"), cancellable = true)
-	public void write(PacketByteBuf buf, CallbackInfo ci) {
-		if (isFootgun()) {
-			buf.writeVarInt(0);
-			ci.cancel();
 		}
 	}
 }
